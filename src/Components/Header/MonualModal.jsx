931007bc3ffb450c5ad3f307d1d498daa4558146
Sh0kh <shoxruuh07tuxtanazarov@gmail.com>
2024-11-07 18:17:54 +0500

    edit

--- src/Components/Header/MonualModal.jsx
+++ src/Components/Header/MonualModal.jsx
@@ -19,7 +19,7 @@ const MonualModal = ({ isOpen, onClose }) => {
       className={`fixed inset-0 bg-[#0000006b] z-50 flex items-center justify-center transition-opacity duration-500 ${isOpen ? 'opacity-100 visible' : 'opacity-0 invisible'}`}
     >
       <div
-        className={`Monual p-[10px] bg-MainColor rounded-[8px] w-[90%] md:w-[50%] lg:w-[50%] py-[30px] transform transition-all duration-500 ${isOpen ? 'scale-100 opacity-100 translate-y-0' : 'scale-75 opacity-0 translate-y-10'}`}
+        className={`Monual p-[10px] bg-MainColor rounded-[8px] w-[95%] md:w-[50%] lg:w-[50%] py-[30px] transform transition-all duration-500 ${isOpen ? 'scale-100 opacity-100 translate-y-0' : 'scale-75 opacity-0 translate-y-10'}`}
         onClick={(e) => e.stopPropagation()}
       >
         <h2 className="text-center text-[white] text-[30px]">Monual</h2>
